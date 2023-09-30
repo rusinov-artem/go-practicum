a4329c7742f794bee4bb2680e5e967c189304207
Rusinov Artem <rusinov.dev@gmail.com>
2023-09-30 20:10:40 +0400

    sprint03 task04

--- sprint03/task04/cmd/code.go
+++ sprint03/task04/cmd/code.go
@@ -1,1 +1,79 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	childernCount, _ := strconv.Atoi(sc.Text())
+
+	greedy := make([]int, 0, childernCount)
+	for i := 0; i < childernCount; i++ {
+		sc.Scan()
+		v, _ := strconv.Atoi(sc.Text())
+		greedy = append(greedy, v)
+	}
+
+	sc.Scan()
+	cookiesCount, _ := strconv.Atoi(sc.Text())
+	cookiesMap := make([]int, 1001)
+
+	for i := 0; i < cookiesCount; i++ {
+		sc.Scan()
+		v, _ := strconv.Atoi(sc.Text())
+		cookiesMap[v]++
+	}
+
+	game := NewGame(greedy, cookiesMap, cookiesCount)
+	fmt.Println(game.happy())
+
+}
+
+type game struct {
+	greedy       []int
+	cookiesMap   []int
+	cookiesCount int
+}
+
+func NewGame(g, cm []int, cc int) *game {
+	return &game{
+		greedy:       g,
+		cookiesMap:   cm,
+		cookiesCount: cc,
+	}
+}
+
+func (t *game) happy() int{
+	count := 0
+	cc := t.cookiesCount
+
+	for _, g := range t.greedy {
+		if cc <= 0 {
+			return count
+		}
+
+		if t.cookiesMap[g] > 0 {
+			t.cookiesMap[g]--
+			cc--
+			count++
+			continue
+		}
+
+		for i := g+1 ; i < 1001 ; i++ {
+			if t.cookiesMap[i] > 0 {
+				t.cookiesMap[i]--
+				cc--
+				count++
+				break
+			}
+		}
+	}
+
+	return count
+}
--- sprint03/task04/cmd/code_test.go
+++ sprint03/task04/cmd/code_test.go
@@ -1,1 +1,92 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CanCreateGame(t *testing.T) {
+	game := NewGame(
+		[]int{10},
+		mkg([]int{}),
+		0,
+	)
+	
+	assert.Equal(t, 0, game.happy())
+}
+
+func Test_CanFindSingleCookie(t *testing.T) {
+	game := NewGame(
+		[]int{10},
+		mkg([]int{10}),
+		1,
+	)
+	
+	assert.Equal(t, 1, game.happy())
+}
+
+func Test_CanFindSingleCookieNext(t *testing.T) {
+	game := NewGame(
+		[]int{10},
+		mkg([]int{100}),
+		1,
+	)
+	
+	assert.Equal(t, 1, game.happy())
+}
+
+func Test_CanFindSingleCookieNextEdge(t *testing.T) {
+	game := NewGame(
+		[]int{10},
+		mkg([]int{1000}),
+		1,
+	)
+	
+	assert.Equal(t, 1, game.happy())
+}
+
+func Test_MoreAssertions(t *testing.T) {
+	assertHappy(t, 2, 
+		[]int{10, 25}, 
+		[]int{1000, 1000},
+	)
+	assertHappy(t, 1, 
+		[]int{10, 25}, 
+		[]int{1000 },
+	)
+	assertHappy(t, 1, 
+		[]int{10, 25}, 
+		[]int{1000, 1 },
+	)
+	assertHappy(t, 0, 
+		[]int{10, 25}, 
+		[]int{1, 1 },
+	)
+	assertHappy(t, 0, 
+		[]int{10, 25}, 
+		[]int{ },
+	)
+	assertHappy(t, 2, 
+		[]int{10, 25}, 
+		[]int{1000, 1000, 1000 },
+	)
+	assertHappy(t, 0, 
+		[]int{}, 
+		[]int{1000, 1000, 1000 },
+	)
+}
+
+func assertHappy(t *testing.T, exp int, g, inp []int) {
+	game := NewGame(g, mkg(inp), len(inp))
+	act := game.happy()
+	assert.Equal(t, exp, act)
+}
+
+func mkg(inp []int) []int {
+	res := make([]int, 1001)
+	for _, v := range inp {
+		res[v]++
+	}
+	return res
+}
