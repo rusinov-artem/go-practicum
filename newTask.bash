32298375dd9f873c546c75a55e1e1c6c75764c11
RusinovArtem <rusinov.dev@gmail.com>
2023-10-08 19:01:43 +0300

    fix newTask.bash

--- newTask.bash
+++ newTask.bash
@@ -4,9 +4,14 @@ echo $1
 
 mkdir -p "$1/cmd"
 
+touch "$1/cmd/code.go"
+touch "$1/cmd/code_test.go"
+touch "$1/cmd/inp.txt"
+
 bdir=`basename $1 | sed 's/\./_/g'`
 
 tmux new-session -c $1 -d -s $bdir \
-    && tmux switch-client -t $bdir || tmux new -c $1 -A -s $bdir #&& \
-   tmux send-keys -t ${bdir}:1.1  "nvim -O \"cmd/code.go\" \"cmd/code_test.go\" \"cmd/inp.txt\"" Enter  
+    && tmux switch-client -t $bdir || tmux new -c $1 -A -s $bdir && \
+   sleep 1 && \
+   tmux send-keys -t ${bdir}:1.1  "nvim -O cmd/code.go cmd/code_test.go cmd/inp.txt" Enter
 
